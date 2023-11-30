2d9c078d61b10a80ea9bddf8a27ba9767eb2f624
Hojoon Kim <hojoon0724@gmail.com>
2023-11-29 20:43:08 -0800

    Commit 1 - I added my first element and it was fun

--- jQuery/hogwarts/app.js
+++ jQuery/hogwarts/app.js
@@ -1,6 +1,7 @@
-console.log("heyo");
-if (typeof jQuery == "undefined") {
-  console.log("oops! I still have to link my jQuery properly!");
-} else {
-  console.log("I did it! I linked jQuery and this js file!");
-}
+$(() => {
+  const $container = $("#container");
+  console.log($container);
+  const $h1 = $("<h1>");
+  $h1.text("Hogwarts");
+  $container.append($h1);
+});
